2802126b55bae34271151b1ef0f1c8cd7b7d5833
Philip Kirkbride <kirkins@gmail.com>
2017-06-17 16:04:55 -0400

    append a unique id to tweets, and put array into object to be sent

--- chrome/src/main.js
+++ chrome/src/main.js
@@ -1,8 +1,14 @@
 var tweetArray = [];
 var regex = /(<([^>]+)>)/ig;
-$(".js-tweet-text-container p").each(function() {
-  var result = $(this).html().replace(regex, "");
-  tweetArray.push(result);
+$(".js-tweet-text-container p").each(function(i) {
+  console.log(i);
+  var tweet = {};
+  tweet.id = i;
+  $(this).attr("id","isis" + i);
+  tweet.text = $(this).html().replace(regex, "");
+  tweetArray.push(tweet);
 });
 
-console.log(tweetArray);
+var tweets_post = {};
+tweets_post.tweets = tweetArray;
+console.log(tweets_post);
